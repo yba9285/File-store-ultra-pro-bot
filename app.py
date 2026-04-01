054ba8feec34cf2b143a88570bb4bb044b253936
yba9285 <108516941+yba9285@users.noreply.github.com>
2026-04-01 23:38:31 +0530

    Configure Flask app with secret key and bot
    Add secret key and initialize bot with Flask

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from web.routes import web
-from config import PORT
+from config import PORT, FLASK_SECRET_KEY
+from bot import bot
+import asyncio
 
 app = Flask(__name__)
+app.secret_key = FLASK_SECRET_KEY
+
 app.register_blueprint(web)
+
+
+# 🔥 START BOT WITH FLASK
+@app.before_first_request
+def start_bot():
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.start())
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
